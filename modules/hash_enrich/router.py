2be465018e274dff7ec96c57c8b8003619f4a7b1
yvsmiinnee <147881214+yasmineadjouadi@users.noreply.github.com>
2026-02-14 00:02:41 +0100

    Update router.py

--- modules/hash_enrich/router.py
+++ modules/hash_enrich/router.py
@@ -5,10 +5,7 @@ router = APIRouter(prefix="/hash", tags=["Hash Enrichment"])
 
 @router.get("/")
 def enrich_hash(param: str = Query(..., description="Hash value to enrich")):
-    """
-    Endpoint pour enrichir un hash via VirusTotal.
-    Exemple: /hash?param=44d88612fea8a8f36de82e1278abb02f
-    """
+    
     vt_data = virustotal_hash(param)
     return {
         "hash": param,
