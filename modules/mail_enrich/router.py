2883f2add3f58dac0f24aca2bcf588f882740e32
yasmineadjouadi <yasmineadjouadi10@gmail.com>
2026-02-25 22:03:57 +0000

    Work before adding domain module

--- modules/mail_enrich/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from modules.mail_enrich.services import get_email_report
-
-router = APIRouter()
-
-@router.get("/mail")
-def enrich_email(email: str):
-    return get_email_report(email)
